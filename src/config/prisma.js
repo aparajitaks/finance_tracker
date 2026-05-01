dedadcb1fa77a3b26c27e3d9334b9cf9c75b4b0d
APARAJITA K SINGH <aparajitaksingh04@gmail.com>
2026-05-01 13:30:06 +0530

    Updated Prisma config to use PostgreSQL adapter with pg Pool

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,10 +1,11 @@
+// Prisma v7 requires a driver adapter for the default "client" engine.
+// This uses @prisma/adapter-pg with the pg Pool driver.
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
 const { Pool } = require("pg");
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
-
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
